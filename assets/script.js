e790e0d059436e6ce70c4c4895cf83aeed2501cf
marquis-cortez99 <127686573+marquis-cortez99@users.noreply.github.com>
2023-04-27 19:14:59 -0500

    changed layout

--- assets/script.js
+++ assets/script.js
@@ -1,97 +1,89 @@
-// Define global variables
-let income = 0;
-let bills = [];
-let annualExpenses = 0;
+// Get all the necessary HTML elements
+const incomeInput = document.getElementById("income-input");
+const incomeBtn = document.getElementById("income-btn");
+const billNameInput = document.getElementById("bill-name");
+const billAmountInput = document.getElementById("bill-amount");
+const billBtn = document.getElementById("bill-btn");
+const budgetList = document.getElementById("budget-list");
+const annualExpenses = document.getElementById("annual-expenses");
+const startOverBtn = document.getElementById("start-over-btn");
+const modalContainer = document.getElementById("modal-container");
+const modalTitle = document.getElementById("modal-title");
+const modalText = document.getElementById("modal-text");
+const modalCloseBtn = document.getElementById("modal-close-btn");
 
+// Set up initial variables
+let monthlyIncome = 0;
+let totalBills = 0;
+let extraIncome = 0;
+let annualExpensesArray = [];
 
-const incomeInput = document.getElementById('income-input');
-const incomeBtn = document.getElementById('income-btn');
-const billName = document.getElementById('bill-name');
-const billAmount = document.getElementById('bill-amount');
-const billBtn = document.getElementById('bill-btn');
-const budgetList = document.getElementById('budget-list');
-const annualExpensesDisplay = document.getElementById('annual-expenses');
-const modalContainer = document.getElementById('modal-container');
-const modalTitle = document.getElementById('modal-title');
-const modalText = document.getElementById('modal-text');
-const modalCloseBtn = document.getElementById('modal-close-btn');
+// Set up event listeners for the income and bill buttons
+incomeBtn.addEventListener("click", addIncome);
+billBtn.addEventListener("click", addBill);
 
-// Add event listeners
-incomeBtn.addEventListener('click', addIncome);
-billBtn.addEventListener('click', addBill);
-modalCloseBtn.addEventListener('click', closeModal);
-
-
-loadData();
-
-// Define functions
+// Define the addIncome function
 function addIncome() {
-  const amount = parseFloat(incomeInput.value);
-  if (!isNaN(amount) && amount > 0) {
-    income += amount;
-    updateBudgetDisplay();
-    saveData();
+  const incomeAmount = Number(incomeInput.value);
+  if (!isNaN(incomeAmount) && incomeAmount > 0) {
+    monthlyIncome += incomeAmount;
+    updateBudget();
   } else {
-    showModal('Invalid input', 'Please enter a valid positive number.');
+    showModal("Invalid Input", "Please enter a valid monthly income.");
   }
-  incomeInput.value = '';
+  incomeInput.value = "";
 }
 
+// Define the addBill function
 function addBill() {
-  const name = billName.value.trim();
-  const amount = parseFloat(billAmount.value);
-  if (name !== '' && !isNaN(amount) && amount > 0) {
-    bills.push({
-      name: name,
-      amount: amount
-    });
-    updateBudgetDisplay();
-    saveData();
+  const billName = billNameInput.value.trim();
+  const billAmount = Number(billAmountInput.value);
+  if (billName !== "" && !isNaN(billAmount) && billAmount > 0) {
+    totalBills += billAmount;
+    updateBudget();
+    const billItem = document.createElement("li");
+    billItem.textContent = `${billName}: $${billAmount}`;
+    budgetList.appendChild(billItem);
   } else {
-    showModal('Invalid input', 'Please enter a valid bill name and a valid positive number for the amount.');
+    showModal("Invalid Input", "Please enter a valid bill name and amount.");
   }
-  billName.value = '';
-  billAmount.value = '';
+  billNameInput.value = "";
+  billAmountInput.value = "";
 }
 
-function updateBudgetDisplay() {
-  let totalBills = 0;
-  bills.forEach(function(bill) {
-    totalBills += bill.amount;
-  });
-  const extraIncome = income - totalBills;
-  budgetList.innerHTML = `
-    <li>Income: $${income.toFixed(2)}</li>
-    <li>Total Bills: $${totalBills.toFixed(2)}</li>
-    <li>Extra Income: $${extraIncome.toFixed(2)}</li>
-  `;
-  annualExpenses = totalBills * 12;
-  annualExpensesDisplay.textContent = `Annual Expenses: $${annualExpenses.toFixed(2)}`;
-}
-
-function saveData() {
-  localStorage.setItem('income', income);
-  localStorage.setItem('bills', JSON.stringify(bills));
-}
-
-function loadData() {
-  const savedIncome = localStorage.getItem('income');
-  if (savedIncome !== null) {
-    income = parseFloat(savedIncome);
+// updateBudget function
+function updateBudget() {
+  const extraIncome = monthlyIncome - totalBills;
+  const incomeItem = budgetList.children[0];
+  incomeItem.textContent = `Income: $${monthlyIncome}`;
+  const billsItem = budgetList.children[1];
+  billsItem.textContent = `Total Bills: $${totalBills}`;
+  const extraIncomeItem = budgetList.children[2];
+  extraIncomeItem.textContent = `Extra Income: $${extraIncome}`;
+  if (extraIncome >= 0) {
+    extraIncomeItem.classList.remove("negative");
+  } else {
+    extraIncomeItem.classList.add("negative");
   }
-  const savedBills = localStorage.getItem('bills');
-  if (savedBills !== null) {
-    bills = JSON.parse(savedBills);
+  const annualExpensesTotal = totalBills * 12;
+  annualExpenses.textContent = `Annual Expenses: $${annualExpensesTotal}`;
+}
+
+
+// Event listener for the start over button
+startOverBtn.addEventListener("click", startOver);
+
+// Define the startOver function
+function startOver() {
+  monthlyIncome = 0;
+  totalBills = 0;
+  extraIncome = 0;
+  annualExpensesArray = [];
+  budgetList.children[0].textContent = "Income: $0";
+  budgetList.children[1].textContent = "Total Bills: $0";
+  budgetList.children[2].textContent = "Extra Income: $0";
+  while (budgetList.children.length > 3) {
+    budgetList.removeChild(budgetList.lastChild);
   }
-  updateBudgetDisplay();
-}
-
-function showModal(title, text) {
-  modalTitle.textContent = title;
-  modalText.textContent = text;
-  modalContainer.style.display = 'block';
-}
-
-function closeModal() {
-  modalContainer.style.display = 'none';
+  annualExpenses.textContent = "0.00";
 }
