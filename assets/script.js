9bc5033df0f8b222edc04583e2f8d4c6d69a23aa
marquis-cortez99 <127686573+marquis-cortez99@users.noreply.github.com>
2023-05-03 23:29:20 -0500

    pushing work up

--- assets/script.js
+++ assets/script.js
@@ -112,7 +112,6 @@ function subtractSavings() {
 function updateProgressBar() {
   const progressPercent = (currentSavings / savingsGoal) * 100 || 0;
   progressBar.style.width = `${progressPercent}%`;
-  progressBar.textContent = `${progressPercent.toFixed(2)}%`;
 }
 
 function resetSavingsGoal() {
@@ -167,4 +166,7 @@ function addBill() {
   billNameInput.value = "";
   billAmountInput.value = "";
 }
+
+
+
 
